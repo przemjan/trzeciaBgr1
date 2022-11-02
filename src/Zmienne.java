779a3ce7412b1855acc8f499013698d3a7f789a3
przemjan <przemjanisz2@gmail.com>
2022-11-02 08:31:03 +0100

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,37 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba1;
+        liczba1 = 123;
+
+        double liczba2 = 23.9;
+
+        System.out.println(liczba1);
+
+        liczba1 = (int)liczba2;
+
+        System.out.println(liczba1);
+
+        String napis = "Lubię \"białko\" :)";
+
+        System.out.println("Mój napis: " + napis);
+        System.out.print("Mój\n napis: " + napis + "\n");
+        System.out.print("Mój\t napis: " + napis);
+
+        int hours = 8;
+        int days = 5;
+        int weeks =52;
+
+        // komentarz
+
+        int workHoursPerYear  = hours * days * weeks;
+
+        System.out.println(workHoursPerYear);
+
+        char znak = '#';
+
+        boolean twierdzenie = false; //true
+
+        //git.scm
+    }
+}
